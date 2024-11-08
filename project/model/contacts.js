b5bac38156ba861ac531cfc231d27aa75587a59a
DC-CC-21 <handstands1000@gmail.com>
2024-11-08 16:28:47 -0500

    added comments to routes

--- project/model/contacts.js
+++ project/model/contacts.js
@@ -1,33 +1,72 @@
+/**
+ * Module for interacting with the contacts collection in the MongoDB
+ * database.
+ */
 const { MongoClient, ObjectId } = require("mongodb");
 const client = new MongoClient(process.env.MONGO_URI);
 
+/**
+ * Contacts collection.
+ * @type {Object}
+ */
 const contacts = {};
 
+
+/**
+ * Get all contacts from the database.
+ * @returns {Promise<Array<Object>>} - An array of all the contacts in the
+ * database.
+ */
 contacts.getAll = async function () {
   try {
+    // Connect to the database.
     await client.connect();
     const db = client.db("cse341");
     const collection = db.collection("project");
+
+    // Find all the documents in the collection.
     const result = await collection.find({}).toArray();
+
+    // Close the connection to the database.
     client.close();
+
     return result;
   } catch (e) {
     console.log(e);
   }
+
   return null;
 };
+
+
+/**
+ * Get a specific contact by id.
+ * @param {string} id - The id of the contact to retrieve.
+ * @returns {Promise<Object>} - The contact with the given id.
+ */
 contacts.getById = async function (id) {
   try {
+    // Connect to the database.
     await client.connect();
     const db = client.db("cse341");
     const collection = db.collection("project");
-    const result = await collection.findOne({ _id: new ObjectId(id) });
+
+    // Create an ObjectId from the given id string.
+    const objectId = ObjectId.createFromHexString(id);
+
+    // Find the document with the given ObjectId.
+    const result = await collection.findOne({ _id: objectId });
+
+    // Close the connection to the database.
     client.close();
+
     return result;
   } catch (e) {
     console.log(e);
   }
+
   return null;
 };
 
 module.exports = contacts;
+
--- project/routes/contacts.js
+++ project/routes/contacts.js
@@ -1,17 +1,39 @@
+/**
+ * This module handles the contacts route
+ * @module routes/contacts
+ * @requires express
+ * @requires ./model/contacts
+ */
+const express = require("express");
+const contactsRoute = express.Router();
 const contacts = require("../model/contacts");
 
-
-
-const contactsRoute = require("express").Router()
-
+/**
+ * GET /contacts/all
+ * Get all the contacts in the database
+ * @name Get all contacts
+ * @function
+ * @param {express.Request} req - The request object
+ * @param {express.Response} res - The response object
+ * @returns {Promise<void>}
+ */
 contactsRoute.get("/all", async (req, res) => {
     const data = await contacts.getAll();
-    res.json(data)
-})
+    res.json(data);
+});
+
+/**
+ * GET /contacts/id/:id
+ * Get a specific contact by id
+ * @name Get contact by id
+ * @function
+ * @param {express.Request} req - The request object
+ * @param {express.Response} res - The response object
+ * @returns {Promise<void>}
+ */
 contactsRoute.get("/id/:id", async (req, res) => {
-    const data = await contacts.getById(req.params.id)
-    res.json(data)
-})
+    const data = await contacts.getById(req.params.id);
+    res.json(data);
+});
 
-
-module.exports = contactsRoute
+module.exports = contactsRoute;
