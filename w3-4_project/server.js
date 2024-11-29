e0b5ff90b6c959ad4a1d2730316b042be746a91b
DC-CC-21 <handstands1000@gmail.com>
2024-11-29 13:42:34 -0500

    updated config base url

--- w3-4_project/server.js
+++ w3-4_project/server.js
@@ -22,7 +22,7 @@ const config = {
   authRequired: false,
   auth0Logout: true,
   secret: process.env.AUTH0_SECRET,
-  baseURL: "http://localhost:8000",
+  baseURL: process.env.BASE_URL,
   clientID: process.env.AUTH0_CLIENT_ID,
   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
 };
